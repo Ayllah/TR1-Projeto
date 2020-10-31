0dec4eccd34bc2f97cff1e5ef9276292006fa731
Ayllah <32244294+Ayllah@users.noreply.github.com>
2020-10-30 22:01:44 -0300

    Update camadafisica.h

--- source/camadafisica.h
+++ source/camadafisica.h
@@ -1,20 +1,31 @@
 #ifndef CAMADAFISICA_H
 #define CAMADAFISICA_H
 
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <stdio.h>
+#include <sstream>
+#include <stdlib.h>
+#include <bits/stdc++.h> 
+
+
 /*************************************************************
 * Camada Transmissora
 *********************************************************** */
 
-int *CamadaFisicaTransmissoraCodificacaoBinaria (int *quadro);
-int *CamadaFisicaTransmissoraCodificacaoManchester (int *quadro);
-int *CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(int *quadro);
+std::vector<int> CamadaFisicaTransmissoraCodificacaoBinaria (std::bitset<8> quadro);
+std::vector<int> CamadaFisicaTransmissoraCodificacaoManchester (std::bitset<8> quadro);
+std::vector<int> CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(std::bitset<8> quadro);
 
 /*************************************************************
 * Camada Receptora
 *********************************************************** */
 
-int *CamadaFisicaReceptoraCodificacaoBinaria (int *quadro);
-int *CamadaFisicaReceptoraCodificacaoManchester (int *quadro);
-int *CamadaFisicaReceptoraCodificacaoManchesterDiferencial(int *quadro);
+std::vector<int> CamadaFisicaReceptoraDecodificacaoBinaria (std::vector<int> quadro);
+std::vector<int> CamadaFisicaReceptoraDecodificacaoManchester (std::vector<int> quadro);
+std::vector<int> CamadaFisicaReceptoraDecodificacaoManchesterDiferencial(std::vector<int> quadro);
 
 #endif 	//CAMADAFISICA_H
